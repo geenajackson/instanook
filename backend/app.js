28019255d785bc35b417d5954a59b2eea7be93e6
Geena Jackson <geena.jackson96@gmail.com>
2023-05-03 10:20:43 -0500

    add g404 error and generic error handlers

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const express = require("express");
 //Dependencies for middleware
 const cors = require("cors");
 const morgan = require("morgan");
+const { NotFoundError } = require("./expressError");
 
 //initialize app
 const app = express();
@@ -14,5 +15,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
+
+
+//404 error to match all other routes
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+//generic error handler to catch any misc. errors
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.log(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
 
 module.exports = app;
