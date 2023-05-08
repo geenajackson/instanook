90765de062b8216508c180cbc15e94021ffbc0cc
Geena Jackson <geena.jackson96@gmail.com>
2023-05-08 11:40:59 -0500

    add data for user_listings and change format of integers

--- backend/instanook-seed.sql
+++ backend/instanook-seed.sql
@@ -1,38 +1,48 @@
--- both test users have the password "password"
+-- both test users have the password 'password'
 
 INSERT INTO users (id, username, password, email, friend_code)
-VALUES ('1',
+VALUES (1,
         'testuser1',
         '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
         'test1@test.com',
         'SW-1111-1111-1111'),
-        ('2',
+        (2,
         'testuser2',
         '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
         'test2@test.com',
         'SW-2222-2222-2222');
 
 INSERT INTO items (id, file_name, type, name)
-VALUES ('1',
+VALUES (1,
         'ant00',
         'villagers',
         'Cyrano'),
-        ('2',
+        (2,
         'common_butterfly',
         'bugs',
         'common butterfly'),
-        ('3',
+        (3,
         'bitterling',
         'fish',
         'bitterling');
 
-INSERT INTO listings (user_id, item_id, price)
-VALUES ('1',
-        '1',
+INSERT INTO listings (id, user_id, item_id, price)
+VALUES (1,
+        1,
+        1,
         100000),
-        ('1',
-        '2',
+        (2,
+        1,
+        2,
         500),
-        ('2',
-        '3',
-        250)
+        (3,
+        2,
+        3,
+        250);
+
+INSERT INTO user_listings (id, user_id, listing_id, listing_type)
+VALUES (1, 1, 1, 'curr'),
+        (2, 1, 2, 'sold'),
+        (3, 2, 2, 'bought'),
+        (4, 2, 3, 'curr'),
+        (5, 1, 3, 'cart');
