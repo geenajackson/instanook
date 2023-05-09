fdfd59e7e695c718fe5911cb5857adc21e0904a1
Geena Jackson <geena.jackson96@gmail.com>
2023-05-09 11:59:57 -0500

    add getById method and include NotFoundError

--- backend/models/item.js
+++ backend/models/item.js
@@ -4,21 +4,22 @@ const axios = require("axios")
 
 const ACNH_API_URL = "https://acnhapi.com/v1"
 const db = require("../db");
-const { BadRequestError } = require("../expressError")
+const { BadRequestError, NotFoundError } = require("../expressError")
 
 /**Related functions for items */
 
 class Item {
     /**Check for item in database
      * 
-     * If item is found, returns {fileName, type, name}
+     * If item is found, returns {id, fileName, type, name}
      * 
      * If not, returns false.
      */
 
     static async check(fileName) {
         const result = await db.query(
-            `SELECT file_name AS "fileName",
+            `SELECT id,
+                    file_name AS "fileName",
                     type,
                     name
             FROM items
@@ -37,7 +38,7 @@ class Item {
      * 
      * Given a filename and a type, add checks the ACNH API to see if the item exists; if it does, it is added.
      * 
-     * Returns {fileName, type, name}.
+     * Returns {id, fileName, type, name}.
      * 
      * If item cannot be found, returns BadRequestError.
     */
@@ -50,7 +51,7 @@ class Item {
                 type,
                 name)
                 VALUES ($1, $2, $3)
-                RETURNING file_name AS "fileName", type, name`,
+                RETURNING id, file_name AS "fileName", type, name`,
                 [fileName, type, res.data.name['name-USen']],
             );
 
@@ -61,8 +62,35 @@ class Item {
         catch (e) {
             throw new BadRequestError(`Item not found: ${fileName}`)
         }
-
     }
+
+    /**Gets item in database via id
+     * 
+     * If item is found, returns {id, fileName, type, name}
+     * 
+     * If not, returns NotFoundError.
+     */
+
+    static async getById(id) {
+        try {
+            const result = await db.query(
+                `SELECT id,
+                        file_name AS "fileName",
+                        type,
+                        name
+                FROM items
+                WHERE id = $1`,
+                [id],
+            );
+
+            const item = result.rows[0];
+
+            return item;
+        }
+        catch (e) {
+            throw new NotFoundError(`Item not found: ${id}`)
+        }
+    };
 };
 
 module.exports = Item;
