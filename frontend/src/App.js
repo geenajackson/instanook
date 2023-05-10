f15af9cd1df3fc3da72db08736dab12257ba8668
Geena Jackson <geena.jackson96@gmail.com>
2023-05-10 10:42:28 -0500

    begin setup of app, including Routes comp and useEffect for user info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import decode from "jwt-decode"
+import './styles/App.css';
+
+import Routes from "./Routes";
+import UserContext from "./UserContext";
+import useLocalStorage from "./useLocalStorage"
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useLocalStorage("token");
+
+  useEffect(
+    function loadUser() {
+      async function getUser() {
+        setIsLoading(true);
+        if (token) {
+          try {
+            let { username } = decode(token);
+          }
+          catch (e) {
+            console.log(e);
+          }
+        }
+        else {
+          setUser(null);
+        };
+      }
+      getUser();
+      setIsLoading(false);
+    }, [token]);
+
+  async function loginUser(login) {
+    try {
+
+    }
+    catch (e) {
+      return e;
+    }
+
+  }
+
+  async function registerUser(login) {
+    try {
+
+    }
+    catch (e) {
+      return e;
+    }
+  }
+
+  function logoutUser() {
+
+    setUser(null);
+  }
+
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={user}>
+        <Routes loginUser={loginUser} logoutUser={logoutUser} registerUser={registerUser} />
+      </UserContext.Provider>
     </div>
   );
 }
