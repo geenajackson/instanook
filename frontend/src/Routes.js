36c3376b430be2b600651e3657c513ed9ab62078
Geena Jackson <geena.jackson96@gmail.com>
2023-05-10 10:42:55 -0500

    begin adding routes and paths to components

---
+++ frontend/src/Routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import Home from "./components/Home"
+import LoginForm from "./forms/LoginForm";
+import SignupForm from "./forms/SignupForm";
+
+function Routes({ loginUser, logoutUser, registerUser }) {
+    return (
+        <Switch>
+            <Route path="/login">
+                <LoginForm loginUser={loginUser} />
+            </Route>
+            <Route exact path="/signup">
+                <SignupForm registerUser={registerUser} />
+            </Route>
+            <Route exact path="/logout">
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+            <Redirect to="/" />
+        </Switch>
+    )
+}
+
+export default Routes;
