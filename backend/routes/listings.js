16582d1d25716035dfc20f0c80986f36bcfacd14
Geena Jackson <geena.jackson96@gmail.com>
2023-05-24 12:03:52 -0500

    add routes for getting all routes via dynamic query and getting a single route via id

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -2,6 +2,7 @@
 
 /** Routes for listings. */
 
+const express = require("express")
 const jsonschema = require("jsonschema");
 const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn } = require("../middleware/auth")
@@ -28,3 +29,44 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
         return next(e);
     }
 })
+
+/** GET / =>
+ *   { listings: [{ id, username, itemName, itemFileName, itemType, price, timePosted, timeSold, listingType }, ...]}
+ *
+ * Can provide search filter in query:
+   * - itemType
+   * - itemName (will find case-insensitive, partial matches)
+   * - user (will get first matching user)
+   * - maxPrice
+   * - listingType
+ */
+
+router.get("/", async function (req, res, next) {
+    const q = req.query;
+
+    try {
+        const listings = await Listing.findAll(q);
+        return res.json({ listings });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** GET / [id] => { listing }
+ *
+ * Returns {id, username, item, price, timePosted, timeSold, listingType}
+ * where item is {fileName, type, name}
+ *
+ * Authorization required: none
+ */
+router.get("/:id", async function (req, res, next) {
+    try {
+        const listing = await Listing.get(req.params.id);
+        return res.json({ listing });
+    }
+    catch (e) {
+        return next(e);
+    }
+})
+
+module.exports = router;
