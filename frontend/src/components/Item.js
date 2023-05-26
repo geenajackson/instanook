55e6783ad15f3af6f55c99bfb3f039ef1fcb34c5
Geena Jackson <geena.jackson96@gmail.com>
2023-05-26 15:42:20 -0500

    create a form for item submission to create a listing

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Card, CardBody, CardTitle, ListGroup, ListGroupItem, Button } from "reactstrap"
+import { Card, CardBody, CardTitle, ListGroup, ListGroupItem, Form, Label, Input, Button } from "reactstrap"
+import UserContext from "../UserContext";
 
 
-function Item({ id, fileName, type, name }) {
+function Item({ id, fileName, type, name, createListing }) {
+    const INITIAL_STATE = { itemId: id, price: 0 }
+    const [formData, setFormData] = useState(INITIAL_STATE);
     const history = useHistory();
     const imageSrc = `http://acnhapi.com/v1/images/${type}/${fileName}`
+
     async function handleSubmit(evt) {
         evt.preventDefault();
+        await createListing(formData);
         history.push("/listings")
     }
+
+    const handleChange = evt => {
+        const { name, value } = evt.target;
+        setFormData(fData => ({
+            ...fData,
+            [name]: value
+        }));
+    };
 
     return (
         <Card color="light" className="Card">
@@ -19,7 +32,24 @@ function Item({ id, fileName, type, name }) {
                 <ListGroup>
                     <ListGroupItem>Type: {type}</ListGroupItem>
                 </ListGroup>
-                <Button color="primary" onClick={handleSubmit}>Item!</Button>
+                <Form onSubmit={handleSubmit}>
+                    <Input
+                        id="itemId"
+                        name="itemId"
+                        type="hidden"
+                        value={id}
+                        onChange={handleChange}
+                    ></Input>
+                    <Label htmlFor="price">Price: </Label>
+                    <Input
+                        id="price"
+                        name="price"
+                        type="number"
+                        value={formData.price}
+                        onChange={handleChange}
+                    ></Input>
+                    <Button type="submit">Create Listing!</Button>
+                </Form>
             </CardBody>
         </Card>
     )
