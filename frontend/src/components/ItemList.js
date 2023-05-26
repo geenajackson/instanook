765234131b36288fe4f871cb80996279a70ccd21
Geena Jackson <geena.jackson96@gmail.com>
2023-05-26 15:41:53 -0500

    add function for creating a listing

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -1,12 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import InstanookApi from "../api";
 import SearchForm from "../forms/SearchForm";
 import Item from "./Item";
+import UserContext from "../UserContext";
 
 function ItemList() {
+    const user = useContext(UserContext);
     const [isLoading, setIsLoading] = useState(true);
     const [items, setItems] = useState([]);
+
+    async function createListing(data) {
+        try {
+            console.log("in itemlist", data)
+            let res = await InstanookApi.createListing(user.id, data);
+            console.log(res)
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
 
     async function fetchData(query = undefined) {
         setIsLoading(true);
@@ -36,6 +49,7 @@ function ItemList() {
                     fileName={item.fileName}
                     type={item.type}
                     name={item.name}
+                    createListing={createListing}
                 ></Item>
             ))}
         </div>
