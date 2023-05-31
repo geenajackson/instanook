b59dc53a8db94ad05a428cc1e6aba5eeff49e68d
Geena Jackson <geena.jackson96@gmail.com>
2023-05-31 13:22:57 -0500

    modify format of time posted/sold and remove cart button if item is already bought

--- frontend/src/components/Listing.js
+++ frontend/src/components/Listing.js
@@ -1,17 +1,23 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Card, CardBody, CardTitle, ListGroup, ListGroupItem, Button } from "reactstrap"
+import moment from "moment";
 
 import InstanookApi from "../api";
 import UserContext from "../UserContext";
 import CartContext from "../CartContext";
 
+import "../styles/Card.css"
 
-function Listing({ id, itemName, itemFileName, itemType, price, timePosted, timeSold }) {
+
+function Listing({ id, itemName, itemFileName, itemType, price, timePosted, timeSold, listingType }) {
     const user = useContext(UserContext);
     const cart = useContext(CartContext);
     const imageSrc = `http://acnhapi.com/v1/images/${itemType}/${itemFileName}`
     const listingUrl = `listings/${id}`
+
+    const datePosted = moment(timePosted, "YYYY-MM-DDTHH:mm").format("l h:mma");
+    const dateSold = moment(timeSold, "YYYY-MM-DDTHH:mm").format("l h:mma");
 
     async function addToCart(evt) {
         evt.preventDefault();
@@ -29,17 +35,18 @@ function Listing({ id, itemName, itemFileName, itemType, price, timePosted, time
         <Card color="light" className="Card">
             <CardBody>
                 <CardTitle tag="h3">
-                    <Link to={listingUrl}>
+                    <Link className="Card" to={listingUrl}>
                         <img alt={itemName} src={imageSrc} width="25%" height="25%" />
                         {itemName}
                     </Link></CardTitle>
                 <ListGroup>
-                    <ListGroupItem>Price: {price}</ListGroupItem>
-                    <ListGroupItem>Time Posted: {timePosted}</ListGroupItem>
-                    {timeSold ? <ListGroupItem>Time Sold: {timeSold}</ListGroupItem> : ""}
+                    <ListGroupItem>Price: {price} bells</ListGroupItem>
+                    <ListGroupItem>Time Posted: {datePosted}</ListGroupItem>
+                    {timeSold ? <ListGroupItem>Time Sold: {dateSold}</ListGroupItem> : ""}
                 </ListGroup>
-                {cart.find(c => c.listingId === id) ? <Button color="secondary" onClick={removeFromCart}>In Cart!</Button> :
-                    <Button color="primary" onClick={addToCart}>Add to Cart</Button>}
+                {listingType === "bought" ? "" :
+                    cart.find(c => c.listingId === id) ? <Button color="secondary" onClick={removeFromCart}>In Cart!</Button> :
+                        <Button color="primary" onClick={addToCart}>Add to Cart</Button>}
             </CardBody>
         </Card>
     )
