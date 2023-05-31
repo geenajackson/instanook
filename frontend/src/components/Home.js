c1db05a00de4fa1368e8ec3062f5a35bd09a99ff
Geena Jackson <geena.jackson96@gmail.com>
2023-05-31 13:23:50 -0500

    render listings on homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,22 @@
 import React, { useContext } from "react";
 import UserContext from "../UserContext";
 import { Link } from "react-router-dom";
+import ListingList from "./ListingList";
+import { Button } from "reactstrap";
+
+import "../styles/Home.css"
 
 function Home() {
     const user = useContext(UserContext);
 
     return (
         <div>
-            {user ? <p>Hello, {user.username}!</p> :
+            {user ? <p>Hello, {user.username}!
+                <ListingList></ListingList></p> :
                 <div>
                     <p>Signup or Login to utilize Instanook!</p>
-                    <button><Link to="/signup">Signup!</Link></button>
-                    <button><Link to="/login">Login</Link></button>
+                    <Button><Link to="/signup">Signup!</Link></Button>
+                    <Button><Link to="/login">Login</Link></Button>
                 </div>}
         </div>
     )
